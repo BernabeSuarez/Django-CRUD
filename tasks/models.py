3963537d5e6eedc19a5d040943e36db929ee0c39
Bernabe <bernatattoo@gmail.com>
2023-11-29 00:49:37 -0300

    Add create Tasks

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=250, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    datecompleted = models.DateTimeField(null=True)
+    important = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        # mostrar los titulos de las tareas
+        return self.title + " de " + self.user.username
