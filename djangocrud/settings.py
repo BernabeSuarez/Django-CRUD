6bb624e6b430c33fe5e424f04ede555bb7b205c6
Bernabe <bernatattoo@gmail.com>
2023-12-01 00:49:03 -0300

    conn_healt_checks True

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -87,6 +87,7 @@ DATABASES = {
         # Feel free to alter this value to suit your needs.
         default="postgresql://postgres:postgres@localhost:5432/mysite",
         conn_max_age=600,
+        conn_health_checks=True,
     )
 }
 
