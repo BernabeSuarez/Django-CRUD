98441bcbf0f08effac83c0949777ecccc62427d8
Bernabe <bernatattoo@gmail.com>
2023-12-01 01:08:27 -0300

    add migration

--- build.sh
+++ build.sh
@@ -3,7 +3,9 @@
 set -o errexit
 
 # poetry install
-# pip install -r requirements.txt
+pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# instalar las dependencias y hacer las migraciones para la base de datos sino no funciona
